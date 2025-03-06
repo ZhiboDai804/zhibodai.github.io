f2717c88e4677a1266f5bee218196306b598e8e1
Hao Wu <hao.wu@khitezza.com>
2025-03-06 23:07:02 +0800

    chore: Optimized interface and redundant css styles
    Signed-off-by: Hao Wu <hao.wu@khitezza.com>

--- assets/assets/js/search.js
+++ assets/assets/js/search.js
@@ -15,9 +15,8 @@ postsSearch = function () {
         return;
     }
 
-    const regex = new RegExp(query, 'gi'); // 全局匹配 + 忽略大小写
+    const regex = new RegExp(query, 'gi');
 
-    // 过滤结果，排除 /about 和 /search 页面
     const results = indexData.filter(item => {
         const isExcluded = item.permalink.includes('/about') || item.permalink.includes('/search');
         return !isExcluded && (regex.test(item.title) || regex.test(item.content));
@@ -38,15 +37,12 @@ postsSearch = function () {
         let content = item.content;
 
         if (regex.test(item.title)) {
-            // 如果标题匹配，截取内容的前 100 个字符
             content = content.substring(0, 100);
         } else {
-            // 查找内容中匹配的部分
             const match = content.match(new RegExp(`.{0,50}${query}.{0,50}`, 'gi'));
             content = match ? match[0] : '';
         }
 
-        // 高亮匹配的搜索词
         content = content.replace(regex, '<strong>$&</strong>');
 
         contentElement.innerHTML = content;
